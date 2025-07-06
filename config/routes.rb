e78f1a5779e0eb7df2f675298237a19a8c685eb9
braga <gbraga.soria@gmail.com>
2025-07-06 04:16:40 -0400

    feat: Inicia estrutura base com scaffold, bootstrap e autenticação

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  
+  
+  root 'home#index'
+  resources :users
+  resources :readings
+  resources :books
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -11,4 +17,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
 end
